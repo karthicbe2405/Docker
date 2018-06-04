18933f7314751da84abe1e5977fbafb7c469b681
Akash Agrawal <akash.agrawal@maximl.com>
2018-06-04 12:51:56 +0530

    Initial Commit

--- prerequisite-setup.sh
+++ prerequisite-setup.sh
@@ -6,7 +6,7 @@ sudo curl -sL https://deb.nodesource.com/setup_8.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 
 sudo apt-get -y install python-pip nodejs redis-server
-sudo npm install -g typescript @angular/cli --allow-root
+sudo npm install -g typescript @angular/cli@6.0.3 --allow-root
 sudo pip install -U pip setuptools
 sudo pip install -r requirements.txt
 
